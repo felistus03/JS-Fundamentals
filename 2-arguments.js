87534dc9a50f25fd559e99aae5853de61a73cc35
Felistus.03 <ceralister03@gmail.com>
2025-09-24 22:44:03 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,17 @@
-/*
+
 const args =['lister',1,2345];
-*/
-const prompt = require('prompt-sync')();
-const args =  prompt("please enter a value");
+const arg =[];
 
 if (args.length == 0) {
+  console.log('No argument');
+} else if (args.length == 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+
+
+if (arg.length == 0) {
   console.log('No argument');
 } else if (args.length == 1) {
   console.log('Argument found');
