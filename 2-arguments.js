74ebe6a8a9e37e65df44a467f630d4a804fadd20
Felistus.03 <ceralister03@gmail.com>
2025-09-24 22:03:49 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,5 +1,8 @@
-
+/*
 const args =['lister',1,2345];
+*/
+const prompt = require('prompt-sync')();
+const args =  prompt("please enter a value");
 
 if (args.length == 0) {
   console.log('No argument');
